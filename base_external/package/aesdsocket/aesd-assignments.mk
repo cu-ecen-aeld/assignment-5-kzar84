834fe1c7da72e79475c10801ca9e92d764a5f3b6
223044734 <Cameron.Causer@ge.com>
2023-08-14 16:35:37 -0400

    Fixing naming in package .mk file

--- base_external/package/aesdsocket/aesd-assignments.mk
+++ base_external/package/aesdsocket/aesd-assignments.mk
@@ -6,20 +6,20 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '43ab714251e74cf27949ce36f57b0518213348f8'
+AESDSOCKET_VERSION = '43ab714251e74cf27949ce36f57b0518213348f8'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-kzar84.git'
-AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+AESDSOCKET_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-kzar84.git'
+AESDSOCKET_SITE_METHOD = git
+AESDSOCKET_GIT_SUBMODULES = YES
 
-define AESD_ASSIGNMENTS_BUILD_CMDS
+define AESDSOCKET_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # Add aesdsocket and start-stop script
-define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+define AESDSOCKET_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/s99aesdsocket
 endef
